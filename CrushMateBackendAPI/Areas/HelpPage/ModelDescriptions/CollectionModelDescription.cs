3e2634b7b446cf52e01279cd5ce05ffc2154ec4e
Faheemmcfc <faheemkurikkal.mp@gmail.com>
2025-08-20 16:16:04 +0530

    Add project files.

---
+++ CrushMateBackendAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace CrushMateBackendAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ CrushMateBackendAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CrushMateBackendAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
